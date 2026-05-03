c2fb4862e1de1dc23339a60fa17699aa8fcf8f47
pk <p.kwiecien@newmotion.com>
2026-05-03 12:07:17 +0200

    https://www.anchor-lang.com/docs/basics/program-structure#program-attribute

--- programs/sol-anch-elections/src/lib.rs
+++ programs/sol-anch-elections/src/lib.rs
@@ -3,14 +3,26 @@ use anchor_lang::prelude::*;
 declare_id!("GaQ54Jta5EcPTbizuNAA6Cq1DZbxUUVfhTJoHSzC8roF");
 
 #[program]
-pub mod sol_anch_elections {
+mod hello_anchor {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
+        ctx.accounts.new_account.data = data;
+        msg!("Changed data to: {}!", data);
         Ok(())
     }
 }
 
+// accounts required by the program
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 8)]
+    pub new_account: Account<'info, NewAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+// accounts created by the program
+#[account]
+pub struct NewAccount {
+    data: u64,
+}
--- tests/src/test_initialize.rs
+++ tests/src/test_initialize.rs
@@ -1,26 +1,41 @@
 use std::str::FromStr;
 
+use anchor_client::anchor_lang::solana_program;
+use anchor_client::solana_sdk::signature::{Keypair, Signer};
 use anchor_client::{
     solana_sdk::{
         commitment_config::CommitmentConfig, pubkey::Pubkey, signature::read_keypair_file,
     },
     Client, Cluster,
 };
+use std::rc::Rc;
 
 #[test]
 fn test_initialize() {
     let program_id = "GaQ54Jta5EcPTbizuNAA6Cq1DZbxUUVfhTJoHSzC8roF";
     let anchor_wallet = std::env::var("ANCHOR_WALLET").unwrap();
-    let payer = read_keypair_file(&anchor_wallet).unwrap();
+    let payer = Rc::new(read_keypair_file(&anchor_wallet).unwrap());
 
-    let client = Client::new_with_options(Cluster::Localnet, &payer, CommitmentConfig::confirmed());
+    let client = Client::new_with_options(
+        Cluster::Localnet,
+        payer.clone(),
+        CommitmentConfig::confirmed(),
+    );
+
     let program_id = Pubkey::from_str(program_id).unwrap();
     let program = client.program(program_id).unwrap();
 
+    let new_account = Keypair::new();
+
     let tx = program
         .request()
-        .accounts(sol_anch_elections::accounts::Initialize {})
-        .args(sol_anch_elections::instruction::Initialize {})
+        .accounts(sol_anch_elections::accounts::Initialize {
+            new_account: new_account.pubkey(),
+            signer: payer.pubkey(),
+            system_program: solana_program::system_program::ID,
+        })
+        .signer(&new_account)
+        .signer(payer.as_ref())
         .send()
         .expect("");
 
